3b1df98c329dab925dd2c6503332e8f638313fb7
cxrvus <benito.m.rabe@gmail.com>
2024-12-10 13:24:29 +0100

    day10 - progress

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,4 +1,93 @@
+use super::util::vec2::*;
+use map::*;
+
+pub mod map {
+	use super::super::util::vec2::*;
+	use std::fmt::Debug;
+
+	#[derive(Debug)]
+	pub struct Map<T: Debug + PartialEq> {
+		pub width: usize,
+		pub height: usize,
+		pub values: Vec<T>,
+	}
+
+	impl<T: Debug + PartialEq> Map<T> {
+		pub fn in_bounds(&self, pos: &Vec2) -> bool {
+			let Vec2 { x, y } = *pos;
+			x >= 0 && y >= 0 && y < self.height as i32 && x < self.width as i32
+		}
+
+		pub fn at(&self, pos: &Vec2) -> Option<&T> {
+			let Vec2u { x, y } = pos.unsign()?;
+			if self.in_bounds(pos) {
+				Some(&self.values[y * self.width + x])
+			} else {
+				None
+			}
+		}
+
+		pub fn set_at(&mut self, pos: &Vec2, value: T) {
+			if self.in_bounds(pos) {
+				let Vec2u { x, y } = pos.unsign().unwrap();
+				self.values[y * self.width + x] = value;
+			} else {
+				panic!("map index is out of range: {:?} = {:?}", pos, value)
+			}
+		}
+
+		pub fn find_all(&self, target: T) -> Vec<Vec2u> {
+			self.values
+				.iter()
+				.enumerate()
+				.filter(|(_, value)| **value == target)
+				.map(|(i, _)| self.get_pos(i).unwrap())
+				.collect()
+		}
+
+		pub fn get_pos(&self, i: usize) -> Option<Vec2u> {
+			self.values.get(i)?;
+			Some(Vec2u {
+				x: (i % self.width),
+				y: (i / self.width),
+			})
+		}
+	}
+}
+
+impl From<&str> for Map<u8> {
+	fn from(value: &str) -> Self {
+		let lines = value.trim().lines();
+		let height = lines.clone().count();
+		let width = lines.clone().next().unwrap().len();
+		let string = lines.collect::<Vec<&str>>().join("");
+
+		let values = string
+			.chars()
+			.map(|c| c.to_digit(10).unwrap() as u8)
+			.collect();
+
+		Self {
+			height,
+			width,
+			values,
+		}
+	}
+}
+
+fn directions() -> [Vec2; 4] {
+	[
+		Vec2 { x: 1, y: 0 },
+		Vec2 { x: 0, y: 1 },
+		Vec2 { x: 0, y: -1 },
+		Vec2 { x: -1, y: 0 },
+	]
+}
+
 pub fn part1() -> usize {
+	let map = Map::from(INPUT);
+	let trail_heads = map.find_all(0);
+
 	todo!()
 }
 
